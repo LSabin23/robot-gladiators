505cccf911d3227ed0aafe856a280bdd58cb049f
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-14 17:00:05 -0400

    Found workaround for window prompts and lack of console logs. Adjusted while loop to break when user skips then choose next robot for next fight.

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,41 +14,7 @@ var fight = function(enemyName) {
   while (enemyHealth > 0 && playerHealth > 0) {
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.')
 
-    if (promptFight === 'FIGHT' || promptFight === 'fight') {
-      // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-      enemyHealth = enemyHealth - playerAttack
-      // Log a resulting message to the console so that we know it worked, users do not see this
-      console.log(
-        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-      )
-
-      // check enemy's health and display enemy status or value to the user
-      if (enemyHealth <= 0) {
-        window.alert(enemyName + ' has died.')
-        break
-      }
-      else {
-        window.alert(enemyName + ' still has ' + enemyHealth + ' health left.')
-      }
-
-      // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-      playerHealth = playerHealth - enemyAttack
-
-      // Log a resulting message to the console so that we know it worked
-      console.log(
-        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
-      )
-
-      // check player health and display player status or value to the user
-      if (playerHealth <= 0) {
-        window.alert(playerName + ' has died.')
-        break
-      }
-      else {
-        window.alert(playerName + ' still has ' + playerHealth + ' health left.')
-      }
-    }
-    else if (promptFight === 'SKIP' || promptFight === 'skip') {
+    if (promptFight === 'SKIP' || promptFight === 'skip') {
       var confirmSkip = window.confirm('Are you sure you want to quit?')
 
       // if yes (true), leave fight and deduct from playerMoney
@@ -56,11 +22,38 @@ var fight = function(enemyName) {
         window.alert(playerName + ' has chosen to skip this fight. Goodbye!')
         playerMoney = playerMoney - 2
         console.log('playerMoney', playerMoney)
-        // break
+        break
       }
-      else {
-        fight()
-      }
+    }
+
+    enemyHealth = enemyHealth - playerAttack
+    console.log(
+      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+    )
+
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + ' has died.')
+      break
+    }
+    else {
+      window.alert(enemyName + ' still has ' + enemyHealth + ' health left.')
+    }
+
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack
+
+    // Log a resulting message to the console so that we know it worked
+    console.log(
+      enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+    )
+
+    // check player health and display player status or value to the user
+    if (playerHealth <= 0) {
+      window.alert(playerName + ' has died.')
+      break
+    }
+    else {
+      window.alert(playerName + ' still has ' + playerHealth + ' health left.')
     }
   }
 }
